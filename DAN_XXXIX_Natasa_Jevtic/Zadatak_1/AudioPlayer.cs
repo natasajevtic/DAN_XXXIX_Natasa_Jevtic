008dcfb6d5bd80ff364be19622808a329b23a771
natasajevtic <nataly.jevtic.95@gmail.com>
2020-06-25 14:57:20 +0200

    Created class AudioPlayer

---
+++ DAN_XXXIX_Natasa_Jevtic/Zadatak_1/AudioPlayer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Zadatak_1
+{
+    /// <summary>
+    /// This class is responsible for adding songs and ads to collections, and their perfoming.
+    /// </summary>
+    class AudioPlayer
+    {
+        public List<Song> Songs { get; set; } = new List<Song>();
+        public List<string> Ads { get; set; } = new List<string>();
+        /// <summary>
+        /// This method sets id of song and adds song to collection.
+        /// </summary>
+        /// <param name="newSong"></param>
+        public void AddSong(Song newSong)
+        {
+            int maxId = 0;
+            if (Songs.Count > 0)
+            {
+                maxId = Songs.Max(x => x.ID);
+            }
+            newSong.ID = maxId + 1;
+            Songs.Add(newSong);
+            WriteSongsToTxt();
+            Console.WriteLine("Successfully added song.");
+        }
+        /// <summary>
+        /// This method reads songs from file and after that puts them in collection.
+        /// </summary>
+        public void ReadSongsFromTxt()
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(@"../../Music.txt");
+                List<string> list = new List<string>();
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    Song song = new Song();
+                    list = lines[i].Split(' ').ToList();
+                    song.Author = list[0].Trim(':');
+                    song.Name = list[1];
+                    TimeSpan.TryParse(list[2], out TimeSpan interval);
+                    song.Duration = interval;
+                    int maxId = 0;
+                    if (Songs.Count > 0)
+                    {
+                        maxId = Songs.Max(x => x.ID);
+                    }
+                    song.ID = maxId + 1;
+                    Songs.Add(song);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("The file was not found.");
+            }
+        }
+        /// <summary>
+        /// This method writes songs to file.
+        /// </summary>
+        public void WriteSongsToTxt()
+        {
+            StreamWriter writer = new StreamWriter(@"../../Music.txt");
+            string output;
+            foreach (Song song in Songs)
+            {
+                output = song.Author + ": " + song.Name + " " + song.Duration;
+                writer.WriteLine(output);
+            }
+            writer.Close();
+        }
+    }
+}
